deadabc8856f3148d0b38280bc20123ab88f9ca8
gangwarsantosh <santoshgangwarmca@gmail.com>
2026-06-06 11:22:48 +0530

    committed the hello api

---
+++ src/main/java/com/springai/springai/controller/SpringAiController.java
@@ -0,0 +1,15 @@
+package com.springai.springai.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SpringAiController {
+	
+	
+	@GetMapping("/hello")
+	public String hello() {
+		return " Hello spring boot developer";
+	}
+
+}
